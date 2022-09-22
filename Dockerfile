81343bb8963f3d15be982dd2de3fa27813d1ff17
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2022-09-22 16:16:18 +0200

    Tester å justere på dockerfile cmd syntax

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,7 @@ RUN pip3 install -r requirements.txt
 
 USER apprunner
 
-# CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
-CMD ["python3", "run.py"]
+ENTRYPOINT []
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
+# CMD ["python3", "run.py"]
--- main.py
+++ main.py
@@ -9,7 +9,6 @@ init_app_logging()
 logger = get_logger(__name__)
 
 api = API()
-app = api.app
 
 
 consumers = [  # Legg til nye topics her
@@ -21,14 +20,14 @@ consumers = [  # Legg til nye topics her
 ]
 
 
-def main():
+def setup_app():
     try:
         run_database_migrations()
         db = Database()
     except Exception as e:
         logger.error(f'Error while migrating database - {e}. Shutting down')
         return
-    api.set_alive(True)
+
     try:
         for info in consumers:
             create_consumer(
@@ -40,13 +39,13 @@ def main():
             )
         api.set_ready(True)
 
+        return api.app
+
     except Exception as e:
         print(f"Application error: {e}")
         logger.error("Application ended")
         logger.error(e)
         api.set_alive(False)
 
-print("toppnivå")
-if __name__ == "__main__":
-    print("inne i main if")
-    main()
+
+app = setup_app()
