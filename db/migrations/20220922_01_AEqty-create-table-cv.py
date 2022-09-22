9a44a37f32595052d083f3ef12525b773728d945
edvardgb <edvard.bakken@nav.no>
2022-09-22 11:57:23 +0200

    creating table without granting user select privledge

--- db/migrations/20220922_01_AEqty-create-table-cv.py
+++ db/migrations/20220922_01_AEqty-create-table-cv.py
@@ -9,5 +9,5 @@ __depends__ = {}
 steps = [
     step("CREATE TABLE cv (aktorId VARCHAR(32), fødselsdato DATE, postnummer VARCHAR(32), kommunenr VARCHAR(32), synligForArbeidsgiver BOOLEAN, synligForVeileder BOOLEAN, hasCar BOOLEAN, oppfolgingsinformasjon JSON, otherExperience JSON, workExperience JSON, courses JSON, certificates JSON, languages JSON, education JSON, vocationalCertificates JSON, authorizations JSON, driversLicenses JSON, skills JSON, jobWishes JSON, PRIMARY KEY (aktorId))"),
     step("GRANT ALL ON ALL TABLES IN SCHEMA public TO cloudsqliamuser"),
-    step("GRANT SELECT ON ALL TABLES IN SCHEMA public TO statistikk-read-user")
+    # step("GRANT SELECT ON ALL TABLES IN SCHEMA public TO statistikk-read-user")
 ]
