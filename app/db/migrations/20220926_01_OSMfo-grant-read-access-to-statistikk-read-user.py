93fb4e365a36c02a587211bcc54ecdfc4250f99e
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2022-09-26 09:27:08 +0200

    Gir lesetilgang til statistikk-read-user
    Co-authored-by: Edvard Bakken <edvard.bakken@nav.no>

---
+++ app/db/migrations/20220926_01_OSMfo-grant-read-access-to-statistikk-read-user.py
@@ -0,0 +1,11 @@
+"""
+GRANT read-access to statistikk-read-user
+"""
+
+from yoyo import step
+
+__depends__ = {'20220922_02_63EOG-rename-foedselsnummer', '__init__'}
+
+steps = [
+    step("GRANT SELECT ON ALL TABLES IN SCHEMA public TO statistikk-read-user")
+]
