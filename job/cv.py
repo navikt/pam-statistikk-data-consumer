0c967ba51683a9743f8052157ae5975ff0560655
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2022-09-28 11:52:48 +0200

    Husker at jobwishes også er en ting
    Co-authored-by: Edvard Bakken <edvard.bakken@nav.no>

--- job/cv.py
+++ job/cv.py
@@ -13,7 +13,19 @@ def _read_from_db(con: Engine):
     return query_result
 
 
-def _list_to_file(name: str, dataframe: pd.DataFrame):
+def _obj_to_file(name: str, dataframe: pd.DataFrame):
+    for index, row in dataframe.iterrows():
+        aktorid = row["aktorid"]
+        jobwishes = row[name]
+
+        for key, value in jobwishes.items():
+            if key == "startoption":
+                pass
+
+            _list_to_file(key, pd.DataFrame({"aktorid": aktorid, key: value}, index=[0]), "jobwishes")
+
+
+def _list_to_file(name: str, dataframe: pd.DataFrame, directory: str):
     new_list = []
     for index, row in dataframe.iterrows():
         values = row[name]
@@ -24,7 +36,7 @@ def _list_to_file(name: str, dataframe: pd.DataFrame):
 
     new_df = pd.DataFrame(data=new_list)
     logger.info(f"name er ferdig formattert. Klargjør skriving til GCP-bucket")
-    write_to_gcp(f"cv/{name}", new_df)
+    write_to_gcp(f"{directory}/{name}", new_df)
 
 
 def _write_to_files(df):
@@ -57,7 +69,9 @@ def _write_to_files(df):
     ]
     for name in lists:
         logger.info(f"Formatterer {name} dataframe for skriving")
-        _list_to_file(name, df[["aktorid", name]])
+        _list_to_file(name, df[["aktorid", name]], directory="cv")
+
+    _obj_to_file("jobwishes", df[["aktorid", "jobwishes"]])
 
 
 def read_write_cv(con: Engine):
